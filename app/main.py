daec47d5597ffad8bff2fcd0da92ba904060652a
Aishwary <aishwary0212@gmail.com>
2026-06-23 08:48:34 +0530

    file delete after done

--- app/main.py
+++ app/main.py
@@ -9,13 +9,18 @@ UPLOAD_DIR=Path("uploads")
 UPLOAD_DIR.mkdir(exist_ok=True)
 @app.post("/parse-resume")
 async def upload_pdf(file:UploadFile=File(...)):
-    file_path=UPLOAD_DIR/file.filename
-    with open(file_path,"wb") as buffer:
-        shutil.copyfileobj(file.file,buffer)
-    text = extract_text(str(file_path))
-    parsed_data=parse_resume(text)
-    return {
-        "message":"Resume received successfully",
-        "file_name":file.filename,
-        "parsed_data":parsed_data
-    }
+    file_path-None
+    try:
+        file_path=UPLOAD_DIR/file.filename
+        with open(file_path,"wb") as buffer:
+            shutil.copyfileobj(file.file,buffer)
+        text = extract_text(str(file_path))
+        parsed_data=parse_resume(text)
+        return {
+            "message":"Resume received successfully",
+            "file_name":file.filename,
+            "parsed_data":parsed_data
+        }
+    finally:
+        if file_path and file_path.exists():
+            file_path.unlink()
